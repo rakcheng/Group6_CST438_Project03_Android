7bfccd724f5fb4f42068f118880d95583d9051b4
jbarrosoarr <jbarrosoarredondo@csumb.edu>
2022-05-04 21:18:48 -0700

    Created RegisterFragment Created LoginFragment Created/Updated fragment_register.xml Created/Updated fragment_login.xml

---
+++ app/src/main/java/com/groupsix/project3_cst438/fragments/RegisterFragment.java
@@ -0,0 +1,29 @@
+package com.groupsix.project3_cst438.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.groupsix.project3_cst438.R;
+
+
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
